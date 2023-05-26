93939a8af0a781a24c768bfad86b5d2c44ff4b66
qiren <mic14cal14@gmail.com>
2023-05-26 00:30:55 -0400

    stuff

--- config/train_chinese.py
+++ config/train_chinese.py
@@ -1,7 +1,7 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-chinese-char'
+out_dir = 'out-chinese'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10 # don't print too too often
@@ -10,11 +10,10 @@ log_interval = 10 # don't print too too often
 always_save_checkpoint = False
 
 wandb_log = False # override via command line if you like
-wandb_project = 'chinese-char'
+wandb_project = 'chinese'
 wandb_run_name = 'mini-gpt'
 
-dataset = 'chinese_char'
-gradient_accumulation_steps = 1
+dataset = 'chinese'
 batch_size = 64
 block_size = 256 # context of up to 256 previous characters
 
