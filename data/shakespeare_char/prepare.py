c17b68b92310d9d4423f3cff89e35fe16b1dfd51
qiren <mic14cal14@gmail.com>
2023-05-26 13:32:28 -0400

    chinese shakespeare edit

--- data/shakespeare_char/prepare.py
+++ data/shakespeare_char/prepare.py
@@ -10,7 +10,7 @@ import requests
 import numpy as np
 
 # download the tiny shakespeare dataset
-input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
+input_file_path = os.path.join(os.path.dirname(__file__), 'chinese.txt')
 if not os.path.exists(input_file_path):
     data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
     with open(input_file_path, 'w') as f:
